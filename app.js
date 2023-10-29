a06c65aa7f7ff3c86e83a7b84fc2995c7dddbb0a
Dawan Sawyer <dawansawyer@gmail.com>
2023-10-29 19:37:24 -0400

    appended the newElement to the page using appendChild property

--- app.js
+++ app.js
@@ -5,3 +5,4 @@ card.innerHTML = '<em>Everything</em> You Need to Know About Data';
 const communitySection = document.querySelector('.testimonials');
 const newElement = document.createElement('h2');
 newElement.textContent = 'Advice for New Udacity Students';
+community.appendChild(newElement);
