9d55ea9022b40d3e68518eae0d2131515588373a
Dawan Sawyer <dawansawyer@gmail.com>
2023-10-29 19:24:07 -0400

    used the querySelector property to select the testimonials and created a new h2 element using the createElement property

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
 const card = document.querySelector('.card__content--h5');
 
 card.innerHTML = '<em>Everything</em> You Need to Know About Data';
+
+const communitySection = document.querySelector('.testimonials');
+const newElement = document.createElement('h2');
