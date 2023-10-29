848ec982ec9b3151babfd76dda34a297742e30fd
Dawan Sawyer <dawansawyer@gmail.com>
2023-10-29 19:40:09 -0400

    created new html for advice section and appended it after the newElement using insertAdjacentHTML property

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
-const card = document.querySelector('.card__content--h5');
+const card = document.querySelector(".card__content--h5");
 
-card.innerHTML = '<em>Everything</em> You Need to Know About Data';
+card.innerHTML = "<em>Everything</em> You Need to Know About Data";
 
-const communitySection = document.querySelector('.testimonials');
-const newElement = document.createElement('h2');
-newElement.textContent = 'Advice for New Udacity Students';
+const communitySection = document.querySelector(".testimonials");
+const newElement = document.createElement("h2");
+newElement.textContent = "Advice for New Udacity Students";
 community.appendChild(newElement);
+const advice =
+  "<p>Set aside specific blocks of time each week \
+	for your coursework. Add those blocks to your calendar and \
+	set reminders.</p>";
+newElement.insertAdjacentHTML('afterend', adviceString);
